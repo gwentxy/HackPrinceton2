c1fa278d8f65d5378957277fc9c670e4e9ce5517
Gwen Tan Xin Yi <gwen.tan@hotmail.com>
2018-03-31 21:14:27 -0400

    WIP

---
+++ AR-Museum/AR-Museum/Assets/Stop_Animation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop_Animation : MonoBehaviour {
+
+	private AudioSource audioSource;
+	private Animation animation1;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AudioSource audioSource = GetComponent<AudioSource>();
+		Animation animation1 = GetComponent<Animation>();
+	
+		if (!audioSource.isPlaying) {
+			animation1.Stop();
+		}
+	}
+}
